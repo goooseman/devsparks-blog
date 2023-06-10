76eaa899b9495998790733e29737ea2201bcb43f
Alexander Gusman <alexander_gusman@intuit.com>
2023-06-10 19:28:34 +0300

    feat: v11, css is not connected, dark theme disabled

--- generated/shared_dependencies.md
+++ generated/shared_dependencies.md
@@ -1,27 +1,32 @@
 Shared dependencies:
 
-ID names of DOM elements:
-- theme-switch (for theme toggle button)
-- remark42 (for Remark42 comments integration)
-
-Classnames:
-- .container
-- .link-active
-- .hackermans-tip
-- .padawans-playground
-
-Shortcodes:
-- hackermans-tip
-- padawans-playground
-
-Function names:
-- changeTheme (for Remark42 theme synchronization)
-
-Message names:
-- N/A
+Exported variables:
+- background-color
+- text-color
+- border-color
 
 Data schemas:
-- N/A
+- None
 
-Exported variables:
-- N/A
+ID names of DOM elements:
+- theme-switch
+- remark42
+
+Classnames:
+- .layout__header
+- .layout__header_left
+- .layout__header_right
+- .header__theme_switch
+- .layout__container
+- .layout__link__active
+- .section__tip__hackerman
+- .section__tip__padawan
+- .tip__container
+- .tip__title
+- .tip__image
+
+Message names:
+- None
+
+Function names:
+- None
--- generated/themes/devsparks/static/js/theme-switcher.js
+++ generated/themes/devsparks/static/js/theme-switcher.js
@@ -1,21 +1,30 @@
-(function() {
-  const themeSwitch = document.getElementById('theme-switch');
-  const currentTheme = localStorage.getItem('theme') || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-  setTheme(currentTheme);
+const themeSwitch = document.getElementById('theme-switch');
 
-  themeSwitch.addEventListener('click', () => {
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-    if (window.r42) {
-      window.r42.changeTheme(newTheme);
-    }
+function setTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme);
+  document.body.classList.remove('light-theme', 'dark-theme');
+  document.body.classList.add(`${theme}-theme`);
+  themeSwitch.textContent = theme === 'dark' ? '🌞' : '🌒';
+  themeSwitch.setAttribute('aria-label', `Switch to ${theme === 'dark' ? 'light' : 'dark'} theme`);
+}
+
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  setTheme(newTheme);
+}
+
+function initTheme() {
+  const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  setTheme(isDark ? 'dark' : 'light');
+}
+
+initTheme();
+
+if (window.matchMedia) {
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+    setTheme(event.matches ? 'dark' : 'light');
   });
+}
 
-  function setTheme(theme) {
-    document.documentElement.setAttribute('data-theme', theme);
-    document.body.className = theme === 'light' ? 'light-theme' : 'dark-theme';
-    themeSwitch.innerHTML = theme === 'light' ? '🌒' : '🌞';
-    themeSwitch.setAttribute('aria-label', theme === 'light' ? 'Switch to dark theme' : 'Switch to light theme');
-  }
-})();
+themeSwitch.addEventListener('click', toggleTheme);
