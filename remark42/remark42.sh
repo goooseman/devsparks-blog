70d3e06a2a74fe457f2ff054cae4d8c773c61b55
Alexander Gusman <alexander_gusman@intuit.com>
2023-06-16 15:40:27 +0200

    refactor(remark42): use docker run instead of docker-compose to run

---
+++ remark42/remark42.sh
@@ -0,0 +1,16 @@
+docker run -d --name remark42 \
+  --hostname remark42 \
+  --restart always \
+  --env-file .env \
+  -e LETSENCRYPT_HOST=r42.goooseman.dev \
+  -e LETSENCRYPT_EMAIL=goooseman@me.com \
+  -e VIRTUAL_HOST=r42.goooseman.dev \
+  -e REMARK_URL=https://r42.goooseman.dev \
+  -e SECRET \
+  -e SITE=localhost,devsparks.goooseman.dev \
+  -e DEBUG=true \
+  --log-driver json-file \
+  --log-opt max-size=10m \
+  --log-opt max-file=5 \
+  -v $PWD/var:/srv/var \
+  umputun/remark42:latest
