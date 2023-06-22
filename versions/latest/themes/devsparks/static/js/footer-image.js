d26764e50e774dbeea961596fe7a4ab69715d734
Alexander Gusman <alexander_gusman@intuit.com>
2023-06-22 10:38:37 +0300

    feat: v0.1.0 gpt-3.5-turbo-16k-0613, USE_FULL_PROJECT_PROMPT
    https://github.com/smol-ai/developer/pull/105
    DEFAULT_MAX_TOKENS=0

--- versions/latest/themes/devsparks/static/js/footer-image.js
+++ versions/latest/themes/devsparks/static/js/footer-image.js
@@ -1,12 +1,14 @@
-const footerImageHuman = document.getElementById('footer-image-human');
-const footerImageRobot = document.getElementById('footer-image-robot');
+document.addEventListener("DOMContentLoaded", function() {
+  const footerImageHuman = document.getElementById("footer-image-human");
+  const footerImageRobot = document.getElementById("footer-image-robot");
 
-footerImageHuman.addEventListener('mouseover', () => {
-  footerImageHuman.style.display = 'none';
-  footerImageRobot.style.display = 'block';
-});
+  footerImageHuman.addEventListener("mouseover", function() {
+    footerImageHuman.style.display = "none";
+    footerImageRobot.style.display = "block";
+  });
 
-footerImageRobot.addEventListener('mouseout', () => {
-  footerImageRobot.style.display = 'none';
-  footerImageHuman.style.display = 'block';
+  footerImageRobot.addEventListener("mouseout", function() {
+    footerImageRobot.style.display = "none";
+    footerImageHuman.style.display = "block";
+  });
 });
