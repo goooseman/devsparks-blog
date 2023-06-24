8242098f93987918e77bd450272adc4334579b41
Alexander Gusman <alexander_gusman@intuit.com>
2023-06-24 08:44:12 +0300

    fix(remark42): enable telegram auth, telegram and email notification

--- remark42/remark42.sh
+++ remark42/remark42.sh
@@ -7,6 +7,10 @@ docker run -d --name remark42 \
   -e VIRTUAL_HOST=r42.goooseman.dev \
   -e REMARK_URL=https://r42.goooseman.dev \
   -e SECRET \
+  -e AUTH_TELEGRAM=true \
+  -e AUTH_EMAIL_ENABLE=true \
+  -e NOTIFY_ADMINS=email,telegram \
+  -e NOTIFY_USERS=email,telegram \
   -e SITE=localhost,devsparks.goooseman.dev \
   -e DEBUG=true \
   --log-driver json-file \
